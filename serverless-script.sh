ef3df2123f38dc662ac88ff08928c0470b0292dc
mahmod-ali29 <43117883+mahmod-ali29@users.noreply.github.com>
2022-05-17 12:44:35 +0200

    Delete serverless-script.sh

--- serverless-script.sh
+++
@@ -1,7 +0,0 @@
-MYSQLSERVER=$(az mysql flexible-server list --query [*].fullyQualifiedDomainName --output tsv)
-MYSQLUSER=$(az mysql flexible-server list --query [*].administratorLogin --output tsv)
-read -p "Insert the database name:" DBname
-read -p "Insert the MySQL password:" MYSQLPASS 
-mysql --host=$MYSQLSERVER \
-      --user=$MYSQLUSER --password=$MYSQLPASS \
-      -e 'use '${DBname}'; SET FOREIGN_KEY_CHECKS = 0; TRUNCATE posts; SET FOREIGN_KEY_CHECKS = 1;' mysql

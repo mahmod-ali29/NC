98fc49cad91dac25a06b2ce2c2416e6a30acd3a3
mahmod-ali29 <43117883+mahmod-ali29@users.noreply.github.com>
2022-05-17 10:33:04 +0200

    Update main.sh

--- AzureCLI/main.sh
+++ AzureCLI/main.sh
@@ -3,9 +3,9 @@ read -p "Insert the External Subnet to access the Linux Container (e.g.89.1.208.
 read -p "Insert the Rresource Group Name (e.g.RG-ghostblog):" RG
 region1="eastus"
 region2="centralus"
-storageAccountName="storageaccounttestnc11as"
-webapp1="appghostblog-2342-${region1}"
-webapp2="appghostblog-2442-${region2}"
+storageAccountName="storageghostblog$RANDOM"
+webapp1="appghostblog$RANDOM-${region1}"
+webapp2="appghostblog$RANDOM-${region2}"
 
 #create storage account
 az storage account create \
@@ -19,7 +19,7 @@ az storage share create \
 --account-name $storageAccountName \
 --name ghost-fileshare 
 
-AccessKey=$(az storage account keys list --account-name $storageAccountName --query [0].value)
+AccessKey=$(az storage account keys list --account-name $storageAccountName --query [0].value --output tsv)
 
 ##Create a custom Image container
 az acr create \
@@ -123,18 +123,22 @@ az webapp create \
 	--plan "AppServicePlan-${region1}" \
 	--name $webapp1 \
 	--deployment-container-image-name acrbuildcontainer11.azurecr.io/ghost/ghost-alpine:v1 \
-    --https-only true \
+  --https-only true \
 	--docker-registry-server-user $acrloginserver \
 	--docker-registry-server-password $acrloginpassword
+
+az webapp stop --name $webapp1 --resource-group $RG
 
 az webapp create \
 	--resource-group $RG \
 	--plan "AppServicePlan-${region2}" \
 	--name $webapp2 \
 	--deployment-container-image-name acrbuildcontainer11.azurecr.io/ghost/ghost-alpine:v1 \
-    --https-only true \
+  --https-only true \
 	--docker-registry-server-user $acrloginserver \
 	--docker-registry-server-password $acrloginpassword
+
+az webapp stop --name $webapp2 --resource-group $RG
 
 #Enable Application and Container Logging for region1 & region2
 az webapp log config \
@@ -151,17 +155,12 @@ az webapp log config \
 	--docker-container-logging filesystem \
 	--web-server-logging filesystem
 
-#configure App Healthcheck for region1 & region2
+#configure App settings for region1 & region2
 az webapp config set \
 	--resource-group $RG \
 	--name $webapp1 \
 	--generic-configurations '{"healthCheckPath":"/"}' \
 	--ftps-state FtpsOnly
-
-az webapp config appsettings set \
-	--resource-group $RG \
-	--name $webapp1 \
-	--settings WEBSITE_HEALTHCHECK_MAXPINGFAILURES=2
 
 az webapp config set \
 	--resource-group $RG \
@@ -169,16 +168,96 @@ az webapp config set \
 	--generic-configurations '{"healthCheckPath":"/"}' \
 	--ftps-state FtpsOnly
 
+# Create MySQL DB Flexible Server
+az mysql flexible-server create \
+  --location $region1 \
+  --resource-group $RG \
+  --name mysqlghostblog$RANDOM \
+  --admin-user ghostuser \
+  --admin-password Test12345! \
+  --sku-name Standard_B1ms \
+  --tier Burstable \
+  --public-access 0.0.0.0 \
+  --storage-size 20 \
+  --storage-auto-grow Enabled \
+  --version 5.7 \
+  --high-availability ZoneRedundant \
+  --zone 1 \
+  --standby-zone 2
+
+#set the ENV parameter for the MySQL DB
+DBName="myghostdb"
+DBServerName=$(az mysql flexible-server list --query [*].name --output tsv)
+MYSQLSERVER=$(az mysql flexible-server list --query [*].fullyQualifiedDomainName --output tsv)
+MYSQLUSER=$(az mysql flexible-server list --query [*].administratorLogin --output tsv)
+MYSQLPASSWORD="Test12345!"
+
+#Disable secure transport
+az mysql flexible-server parameter set \
+--resource-group $RG \
+--server-name $DBServerName \
+--name require_secure_transport \
+--value OFF
+
+##Create MySQL DB
+az mysql flexible-server db create \
+--resource-group $RG \
+--server-name $DBServerName \
+--database-name $DBName
+
+################################################
+#Set the App settings 
+az webapp config appsettings set \
+	--resource-group $RG \
+	--name $webapp1 \
+	--settings \
+      WEBSITE_HEALTHCHECK_MAXPINGFAILURES=2 \
+      database__client=mysql \
+      database__connection__database=$DBName \
+      database__connection__host=$MYSQLSERVER \
+      database__connection__user=$MYSQLUSER \
+      database__connection__password=$MYSQLPASSWORD
+
 az webapp config appsettings set \
 	--resource-group $RG \
 	--name $webapp2 \
-	--settings WEBSITE_HEALTHCHECK_MAXPINGFAILURES=2
+	--settings \
+      WEBSITE_HEALTHCHECK_MAXPINGFAILURES=2 \
+      database__client=mysql \
+      database__connection__database=$DBName \
+      database__connection__host=$MYSQLSERVER \
+      database__connection__user=$MYSQLUSER \
+      database__connection__password=$MYSQLPASSWORD
+
+az webapp config storage-account add \
+  --resource-group $RG \
+  --name $webapp1 \
+  --account-name $storageAccountName \
+  --custom-id volume \
+  --share-name "ghost-fileshare" \
+  --access-key $AccessKey \
+  --storage-type AzureFiles \
+  --mount-path /var/lib/ghost/content
+
+az webapp config storage-account add \
+  --resource-group $RG \
+  --name $webapp2 \
+  --account-name $storageAccountName \
+  --custom-id volume \
+  --share-name "ghost-fileshare" \
+  --access-key $AccessKey \
+  --storage-type AzureFiles \
+  --mount-path /var/lib/ghost/content  
+
+#start the Web App again
+az webapp start --name $webapp1 --resource-group $RG
+az webapp start --name $webapp2 --resource-group $RG
 
 #####Create Frontdoor using ARM template#######
 az deployment group create \
 	--resource-group $RG \
 	--template-file azuredeploy.json \
-    --parameters frontDoorName="ghostblogafd" backendAddress="$webapp1.azurewebsites.net" backendAddress2="$webapp2.azurewebsites.net"
+  --parameters frontDoorName="ghostblogafd" backendAddress="$webapp1.azurewebsites.net" backendAddress2="$webapp2.azurewebsites.net"
 
 #Create WAF policy and associate it to the frontdoor
 az extension add --name front-door
@@ -207,37 +286,37 @@ az network front-door update \
 az webapp config access-restriction add \
 	--resource-group $RG \
 	--name $webapp1 \
-    --rule-name allow-frontdoor \
-   	 --action Allow \
-   	 --service-tag AzureFrontDoor.Backend \
-	 --priority 10
+  --rule-name allow-frontdoor \
+  --action Allow \
+  --service-tag AzureFrontDoor.Backend \
+	--priority 10
 
 az webapp config access-restriction add \
 	--resource-group $RG \
 	--name $webapp2 \
-    --rule-name allow-frontdoor \
-   	--action Allow \
-   	--service-tag AzureFrontDoor.Backend \
-    --priority 10
+  --rule-name allow-frontdoor \
+  --action Allow \
+  --service-tag AzureFrontDoor.Backend \
+  --priority 10
 
 #Blocking direct access to the App via ssh, allow only from the your External IP Address
  az webapp config access-restriction add \
 	--resource-group $RG \
 	--name $webapp1 \
-    --rule-name allow-frontdoor \
-    --action Allow \
-    --scm-site true \
-    --ip-address $ExternalIP \
-    --priority 10
+  --rule-name allow-frontdoor \
+  --action Allow \
+  --scm-site true \
+  --ip-address $ExternalIP \
+  --priority 10
 
 az webapp config access-restriction add \
 	--resource-group $RG \
 	--name $webapp2 \
-    --rule-name allow-frontdoor \
-    --action Allow \
-    --scm-site true \
-    --ip-address $ExternalIP \
-    --priority 10
+  --rule-name allow-frontdoor \
+  --action Allow \
+  --scm-site true \
+  --ip-address $ExternalIP \
+  --priority 10
 
 #Create Log Analytics 
 az monitor log-analytics workspace create \
@@ -414,6 +493,7 @@ az webapp deployment slot create \
 	--resource-group $RG \
 	--slot testing \
 	--configuration-source $webapp1
+
 az webapp deployment slot create \
 	--name $webapp2 \
 	--resource-group $RG \

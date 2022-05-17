8981be29af582fcb2d5c86a63c2fd22b1ffc815e
mahmod-ali29 <43117883+mahmod-ali29@users.noreply.github.com>
2022-05-17 14:12:05 +0200

    Update main.sh

--- Terraform/main.sh
+++ Terraform/main.sh
@@ -1,19 +1,22 @@
 read -p "Insert the Resource Group Name:" RG
-read -p "Insert the External IP address to access the container (e.g x.x.x.x/x):" ExternalIP
+storageAccountName="storageghostblog$RANDOM"
 
-##Create ACR
+#Get  your External IP address
+ExternalIP=$(curl -s ipinfo.io/ip)
+
+#Create ACR
 az acr create \
-	--resource-group $RG \
-	--name acrbuildcontainer11 \
-	--sku Basic \
-	--admin-enabled true
+        --resource-group $RG \
+        --name acrbuildcontainer11 \
+        --sku Basic \
+        --admin-enabled true
 
 #Build image and push it to ACR
 az acr build \
-	--image ghost/ghost-alpine:v1 \
-	--registry acrbuildcontainer11 \
-	--file Dockerfile . 
+        --image ghost/ghost-alpine:v1 \
+        --registry acrbuildcontainer11 \
+        --file Dockerfile . 
 
 #Run terraform script
 terraform init
-terraform apply -var-file=variable.tfvars -var="RG=$RG" -var="ExternalIP=$ExternalIP" -auto-approve
+terraform apply -var-file=variable.tfvars -var="RG=$RG" -var="ExternalIP=$ExternalIP" -var="storageAccountName=$storageAccountName" -auto-approve
